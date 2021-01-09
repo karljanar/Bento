a81835aeabf84f00028415a43993c862b04a8d17
Karl Janar Kinkar <25798556+karljanar@users.noreply.github.com>
2021-01-09 14:44:14 +0200

    Update greeting.js

--- js/greeting.js
+++ js/greeting.js
@@ -3,15 +3,13 @@ const today = new Date();
 const hour = today.getHours();
 
 // Here you can change your name
-const name = 'John Doe';
+const name = 'Janar';
 
 // Here you can change your greetings
-const gree1 = 'Go to Sleep! ';
-const gree2 = 'Good morning! ';
-const gree3 = 'Good afternoon ';
-const gree4 = 'Good evening ';
-const gree5 = 'Good evening ';
-const gree6 = 'Good evening ';
+const gree1 = 'Nokki ka? ';
+const gree2 = 'Vapsje, juba üleval, ';
+const gree3 = 'Hommik, ';
+const gree4 = 'Pime on, ';
 
 // Define the hours of the greetings
 if (hour >= 23 && hour < 5) {
